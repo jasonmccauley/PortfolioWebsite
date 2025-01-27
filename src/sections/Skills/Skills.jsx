394e9d9660ed0bdf9352b515512a4b2b4e46e895
jasonmccauley <jsonmccauley@gmail.com>
2025-01-27 00:25:10 -0500

    finished styling contact form

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -5,7 +5,7 @@ import SkillList from "../../common/SkillList";
 function Skills() {
   return (
     <section id="skills" className={styles.container}>
-        <h1>Skills</h1>
+        <h1 className="sectionTitle">Skills</h1>
         <div className={styles.skillList}>
           <SkillList src={checkMarkIcon} skill="Python" />
           <SkillList src={checkMarkIcon} skill="Java" />
